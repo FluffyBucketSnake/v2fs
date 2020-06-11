83374d2f588d20b66591f2968b14ec06e20201d5
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2020-06-11 04:03:10 -0400

    Initial super block declaration

---
+++ libv2fs/include/super.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <mount.h>
+
+//
+// Represents a mounted file system and holds the data and operations necessary to
+// operate the file system directly.
+//
+struct super_block
+{
+    const file_system_type *s_fs;   // The file system type.
+    void *s_info;                   // File system implementation specific data.
+};

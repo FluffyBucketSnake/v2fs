5d757f7f054b3443ded497bda36b0dce84e4add0
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2020-06-20 21:06:51 -0400

    Add creation and destruction methods for dentries

--- libv2fs/include/dentry.h
+++ libv2fs/include/dentry.h
@@ -7,6 +7,24 @@
 // holds the data associated with the directory entry itself and not the file.
 //
 struct dentry {
-    const char *d_name;     // The filename of the directory entry.
-    const inode *d_inode;   // The associated inode.
+    const char *d_name;             // The filename of the directory entry.
+    const struct inode *d_inode;    // The associated inode.
+    const size_t d_count;           // The number of references to this dentry.
 };
+
+//
+// Creates a new dentry.
+//
+struct dentry *dnew(const char *filename, const struct inode *d_inode);
+
+//
+// Used when dentry pointer is copied to another variable. Increases the reference count
+// of the dentry.
+//
+struct dentry *dget(struct dentry *dentry);
+
+//
+// Used when a dentry pointer is being disposed off. Decreases the reference count of the
+// dentry and, if it reaches zero, frees it.
+//
+struct dentry *dput(struct dentry *dentry);

e97540e490ed41fd2c5fc3a87ee8609b0c2215ca
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2020-06-20 21:11:59 -0400

    Make fst into a linked list and comment out unregister_filesystem

--- libv2fs/include/mount.h
+++ libv2fs/include/mount.h
@@ -6,17 +6,18 @@ struct super_block;
 // Holds the data of a file system type implementation, such as the mount function.
 //
 struct file_system_type {
-    const char* name;                               // Unique identifier of file system type.
-    super_block *(*mount) (void *data);             // Mounts a filesystem using the specified data 
-    void (*kill_sb) (super_block *super_block);     // Umounts the specified file system.
+    const char* name;                                       // Unique identifier of file system type.
+    struct super_block *(*mount) (void *data);              // Mounts a filesystem using the specified data 
+    void (*kill_sb) (struct super_block *super_block);      // Umounts the specified file system.
+    struct file_system_type *next;                          // Holds the pointer to the next registered entry.
 };
 
 //
 // Registers the file system type into the V2FS library.
 //
-extern int register_filesystem(const struct file_system_type *fs);
+int register_filesystem(const struct file_system_type *fs);
 
 //
 // Unregisters the file system type from the V2FS library.
 //
-extern int unregister_filesystem(const struct file_system_type *fs);
+// int uregister_filesystem(const struct file_system_type *fs);
